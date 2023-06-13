41409c303d3a4f262d8d815814bbaab2142ba579
Katie Busching <katie.busching@gmail.com>
2023-06-13 17:15:30 -0400

    working on Counter

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,9 +1,13 @@
-<!
+// buttons and counter text
+const counter =document.querySelector(#counter)
+const btns = document.querySelectorAll('.btn')
+
+console.log(btns)
 let stash = 0;
 const count = 1;
 let countedDonuts = 0;
 
-
+<script src="images/Donut Pattern.jpg"></script>
 
 
     
