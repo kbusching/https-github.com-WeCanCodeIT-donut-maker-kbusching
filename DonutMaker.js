e2f1c509df9d87e83f62ac35d13b59b244904e6a
Katie Busching <katie.busching@gmail.com>
2023-06-14 18:53:20 -0400

    increase works

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,16 +1,35 @@
-// buttons and counter text
-const counter =document.querySelector(#counter)
-const btns = document.querySelectorAll('.btn')
+//buttons and counter text
 
-console.log(btns)
-let stash = 0;
-const count = 1;
-let countedDonuts = 0;
+//const counter = document.querySelector(counter)
+//const increaseBtn = document.querySelector('btn-increase');
+//const resetBtn = document.querySelector('btn-reset');
+//const decreaseBtn = document.querySelector('btn-decrease');
 
-<script src="images/Donut Pattern.jpg"></script>
+//increaseBtn.addEventListener('click', function(){})
+class DonutMaker {
+    constructor() {
+        this.donutCount = 0;
+        this.autoClickerCount = 0;
+        this.autoClickerCost = 100;
+    }
 
+    addDonut() {
+        this.donutCount += 1;
+    }
 
+    getDonutCount() {
+        return this.donutCount;
+    }
+}
     
-    
+    const donutMaker = new DonutMaker();
+
+const donutButton = document.getElementById('btn-increase');
+const donutCountDisplay = document.getElementById('counter');
 
 
+donutButton.addEventListener('click', () => {
+    donutMaker.addDonut();
+    donutCountDisplay.textContent = donutMaker.getDonutCount();
+    checkDonutsForAutoClicker();
+});
